4e533e39750c6d65a3655faa6b989b0a4f1adf1d
Salil Bhatnagar <salil96ii@gmail.com>
2022-12-02 15:52:27 +0100

    setup file added with modification

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="snowflake",
-    version='0.2',
+    version='0.1',
     author="Salil-23220858",
     author_email="salil.bhatnagar@fau.de",
     packages=find_packages(),
